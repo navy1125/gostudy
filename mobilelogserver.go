4fc9cd110409ca82c3a80324ad1101fcd85e86ae
HaijunWang <navy1125@126.com>
2013-12-09 21:21:02 +0800

    整合服务器活动脚本

--- mobilelogserver.go
+++ mobilelogserver.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"code.google.com/p/go.net/websocket"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/GXTime/logging"
 	"github.com/navy1125/config"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 var (
@@ -47,6 +49,24 @@ func LogServer(w http.ResponseWriter, req *http.Request) {
 	Broadcask([]byte(req.RemoteAddr + req.URL.String() + string(text)))
 }
 
+func testLua(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("/tmp/test.lua")
+	if err != nil {
+		fmt.Println("open test.lua err:", err)
+		return
+	}
+	text, _ := ioutil.ReadAll(file)
+	type LuaData struct {
+		Id    string `json:"id,omitempty"`
+		Value string `json:"value,omitempty"`
+	}
+	ld := LuaData{
+		Id:    "lua",
+		Value: string(text),
+	}
+	b, _ := json.Marshal(ld)
+	w.Write(b)
+}
 func main() {
 	flag.Parse()
 	config.SetConfig("config", *flag.String("config", "config.xml", "config xml file for start"))
@@ -72,6 +92,7 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.HandleFunc("/log/fxsj", LogServer)
 	http.HandleFunc("/log/hxsg", LogServer)
+	http.HandleFunc("/testlua", testLua)
 	http.HandleFunc("/log/sbjs", LogServer)
 	err = http.ListenAndServe(config.GetConfigStr("ip")+":"+config.GetConfigStr("port"), nil)
 	if err != nil {
