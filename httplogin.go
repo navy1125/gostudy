c6cc6ba0d74d873529f27cd9947865981d9ab028
wanghaijun <wanghaijun@ztgame.com>
2015-08-07 15:09:20 +0800

    Post data

--- httplogin.go
+++ httplogin.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -10,9 +12,14 @@ import (
 
 func main() {
 
-	paracheck := "http://127.0.0.1:7000/httplogin?game=101&zone=1&cmd=register-account"
+	paracheck := "http://127.0.0.1:7000/httplogin?game=101&zone=1&cmd=plat-token-login"
 	fmt.Println(paracheck)
-	resp, err := http.Post(paracheck, "application/x-www-form-urlencoded", strings.NewReader(`{"do":"register-account","data":{"gameid":170}}`))
+	hash := md5.New()
+	io.WriteString(hash, "officialwhj@whj.whjmalenavy1125671234532180f47650ba0d2834f54c837d518a8eca")
+	sign := fmt.Sprintf("%x", hash.Sum(nil))
+	fmt.Println(sign)
+	//resp, err := http.Post(paracheck, "application/x-www-form-urlencoded", strings.NewReader(`{"do":"register-account","data":{"gameid":170,"mid":"{\"sdk\":\"official\",\"account\":\"{\\\"iso\\\":\\\"operator\\\",\\\"systemversion\\\":\\\"00\\\",\\\"IMEI\\\":\\\"867255022606737\\\"}\"}"}}}`))
+	resp, err := http.Post(paracheck, "application/x-www-form-urlencoded", strings.NewReader(fmt.Sprintf(`{"do":"plat-token-login","data":{"gameid":170,"platinfo":{"account":"official","platid":"67","email":"whj@whj.whj","gender":"male","nickname":"navy1125","timestamp":"12345","uid":"321","sign":"%s"}}}`, sign)))
 	//resp, err := http.Get(paracheck)
 	if err != nil {
 		log.Fatal("http.Get Err:", err)
