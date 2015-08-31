2cf08838e109104672312d75c0aeea742354d464
wanghaijun <wanghaijun@ztgame.com>
2015-08-31 12:48:08 +0800

    daemon测试

---
+++ daemontest.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	fmt.Println(os.Getppid())
+	if os.Getppid() != 1 {
+		//判断当其是否是子进程，当父进程return之后，子进程会被 系统1 号进程接管
+		filePath, _ := filepath.Abs(os.Args[0])
+		//将命令行参数中执行文件路径转换成可用路径
+		cmd := exec.Command(filePath)
+		cmd.Start()
+		return
+	}
+	http.ListenAndServe(":8000", nil)
+}
--- httplogin.go
+++ httplogin.go
@@ -15,11 +15,13 @@ func main() {
 	paracheck := "http://127.0.0.1:7000/httplogin?game=101&zone=1&cmd=plat-token-login"
 	fmt.Println(paracheck)
 	hash := md5.New()
-	io.WriteString(hash, "officialwhj@whj.whjmalenavy1125671234532180f47650ba0d2834f54c837d518a8eca")
+	io.WriteString(hash, "10722whj@whj.whjmalenavy112567123451072280f47650ba0d2834f54c837d518a8eca")
+	//h5.bwgame.com.cn/SuperSlot/?platid=67&uid=10722&account=10722&email=whj@whj.whj&gender=male&nickname=navy1125&timestamp=12345&sign=54ad6e9d12baac23ed73e5613913a6db
+	//hashstr := plataccount + email + gender + gameid + nickname + platid + timestamp + plataccid + key
 	sign := fmt.Sprintf("%x", hash.Sum(nil))
-	//fmt.Println(sign)
+	fmt.Println(sign)
 	//resp, err := http.Post(paracheck, "application/x-www-form-urlencoded", strings.NewReader(`{"do":"register-account","data":{"gameid":170,"mid":"{\"sdk\":\"official\",\"account\":\"{\\\"iso\\\":\\\"operator\\\",\\\"systemversion\\\":\\\"00\\\",\\\"IMEI\\\":\\\"867255022606737\\\"}\"}"}}}`))
-	str := fmt.Sprintf(`{"do":"plat-token-login","data":{"gameid":170,"platinfo":{"account":"official","platid":"67","email":"whj@whj.whj","gender":"male","nickname":"navy1125","timestamp":"12345","uid":"321","sign":"%s"}}}`, sign)
+	str := fmt.Sprintf(`{"do":"plat-token-login","data":{"gameid":170,"platinfo":{"account":"10722","platid":"67","email":"whj@whj.whj","gender":"male","nickname":"navy1125","timestamp":"12345","uid":"10722","sign":"%s"}}}`, sign)
 	resp, err := http.Post(paracheck, "application/x-www-form-urlencoded", strings.NewReader(str))
 	//resp, err := http.Get(paracheck)
 	if err != nil {
