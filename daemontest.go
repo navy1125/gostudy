67d0c855664485132e5ccf70f4a150d8762a74c4
wanghaijun <wanghaijun@ztgame.com>
2015-08-31 12:58:04 +0800

    daemon测试

--- daemontest.go
+++ daemontest.go
@@ -10,11 +10,12 @@ import (
 
 func main() {
 	fmt.Println(os.Getppid())
+	fmt.Println(os.Args)
 	if os.Getppid() != 1 {
 		//判断当其是否是子进程，当父进程return之后，子进程会被 系统1 号进程接管
 		filePath, _ := filepath.Abs(os.Args[0])
 		//将命令行参数中执行文件路径转换成可用路径
-		cmd := exec.Command(filePath)
+		cmd := exec.Command(filePath, os.Args[1:]...)
 		cmd.Start()
 		return
 	}
